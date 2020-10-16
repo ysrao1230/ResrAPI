9a2e422f9fec5ac2f705fe25ca18976ee3313b6f
ysrao1230 <ysrao1230@gmail.com>
2020-10-16 13:11:30 +0530

    Small changes

--- src/main/java/Sample/sample.java
+++ src/main/java/Sample/sample.java
@@ -24,18 +24,17 @@ public class sample {
 
 	static String name = "Hello World";
 
+	@SuppressWarnings("null")
 	public static void main(String[] args) {
 
 		System.out.println("Given String is: " + name);
 
-	}
+		/*String str = null;
+		System.out.println(String.valueOf(str)); // This will print a String equal to "null"
+		System.out.println(str.toString()); // This will throw a NullPointerException
+*/	}
 
-	@Test(priority=0)
-	void Sample() {
-		System.out.println("this is first testng");
-	}
-
-	@Test(priority=1)
+	@Test(priority = 1)
 	public void lotto_resource_returns_200_with_expected_id_and_winners() {
 		System.out.println("**Starting Getting Body**");
 		given().
@@ -43,90 +42,82 @@ public class sample {
 		then().
 			statusCode(200).
 			body("data.id[1]", equalTo(8)).
-			body("data.first_name", hasItems("Michael", "Lindsay")).
+			body("data.first_name", hasItems("Michael", "Lindsay","Rachel")).
 			log().
 			all();
+		
 		System.out.println("**Ending  Getting Body**");
 
 	}
 
 	@SuppressWarnings("unchecked")
-	@Test(priority=2)
-	public void test2() {
+	@Test(priority = 2)
+	public void post_data() {
 		System.out.println("Starting  of posting Data");
 		JSONObject request = new JSONObject();
 		request.put("name", "chaya");
 		request.put("job", "BA");
 
-		System.out.println(request);
 		System.out.println(request.toString());
 
-		given().
-			body(request.toJSONString()).
-		when().
-			post("https://reqres.in/api/users").
-		then().
-			statusCode(201).
-			log().
-			all();
+		given().body(request.toJSONString()).when().post("https://reqres.in/api/users").then().statusCode(201).log()
+				.all();
 		System.out.println("Ending of posting Data");
 
 	}
-	
 
-	@Test(priority=3)
+	@Test(priority = 3)
 	public void xml_Validation() {
 		System.out.println("************XML VALIDATION****************");
-		 RestAssured.baseURI ="https://samples.openweathermap.org/data/2.5/"; 
-		 RequestSpecification request = RestAssured.given();
-		 
-		 Response response = request.queryParam("q", "London,UK") 
-		                    .queryParam("appid", "2b1fd2d7f77ccf1b7de9b441571b39b8") 
-		                    .get("/weather");
-		 
-		 String jsonString = response.asString();
-		 System.out.println(response.getStatusCode()); 
-		 Assert.assertEquals(jsonString.contains("London"), true);	
-		 
+		RestAssured.baseURI = "https://samples.openweathermap.org/data/2.5/";
+		RequestSpecification request = RestAssured.given();
+
+		Response response = request.queryParam("q", "London,UK").queryParam("appid", "2b1fd2d7f77ccf1b7de9b441571b39b8")
+				.get("/weather");
+
+		String jsonString = response.asString();
+		System.out.println(response.getStatusCode());
+		Assert.assertEquals(jsonString.contains("London"), true);
+
 	}
+
 	@SuppressWarnings("unchecked")
-	@Test(priority=4)
+	@Test(priority = 4)
 	public void test_put() {
 
 		JSONObject request = new JSONObject();
 		request.put("name", "chaya");
 		request.put("job", "BAA");
-
-		System.out.println(request);
 		System.out.println(request.toString());
 
 		given().
 			body(request.toJSONString()).
 		when().
 			put("https://reqres.in/api/users").
-		then().statusCode(200);
+		then().
+			statusCode(200);
 
 	}
 
 	@SuppressWarnings("unchecked")
-	@Test(priority=5)
+	@Test(priority = 5)
 	public void test_patch() {
 
 		JSONObject request = new JSONObject();
 		request.put("name", "chaya");
 		request.put("job", "BAA");
 
-		System.out.println(request);
 		System.out.println(request.toString());
 
 		given().
 			body(request.toJSONString()).
 		when().
 			patch("https://reqres.in/api/users/2").
-			then().statusCode(200);
-
+		then().
+			statusCode(200);
 	}
-	@Test(priority=6)
+
+	@Test(priority = 6)
 	public void test_delete() {
 
 		JSONObject request = new JSONObject();
@@ -134,8 +125,10 @@ public class sample {
 			body(request.toJSONString()).
 		when().
 			delete("https://reqres.in/api/users/2").
-		then().statusCode(204).
-			log().all();
+		then().
+			statusCode(204).
+			log().
+			all();
 		System.out.println("Exit program");
 
 	}
